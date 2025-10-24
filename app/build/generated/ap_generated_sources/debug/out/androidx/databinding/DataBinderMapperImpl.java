3f972bf73216e7150739acc1ebe00cfcf542100f
Nabeel211 <daan.nabil52@gmail.com>
2025-10-24 20:41:56 +0700

    responsi

---
+++ app/build/generated/ap_generated_sources/debug/out/androidx/databinding/DataBinderMapperImpl.java
@@ -0,0 +1,7 @@
+package androidx.databinding;
+
+public class DataBinderMapperImpl extends MergedDataBinderMapper {
+  DataBinderMapperImpl() {
+    addMapper(new com.unsoed.responsi1mobileh1d023100.DataBinderMapperImpl());
+  }
+}
---
+++ app/build/generated/data_binding_trigger/debug/com/unsoed/responsi1mobileh1d023100/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.unsoed.responsi1mobileh1d023100;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
